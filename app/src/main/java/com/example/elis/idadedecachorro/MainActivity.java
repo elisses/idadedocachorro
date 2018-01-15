ca2ef3b62b300a9535eec9fb59df139c0d0f21dc
Elis <fernandalisses@gmail.com>
2018-01-15 20:15:27 +0100

    First Commit

---
+++ app/src/main/java/com/example/elis/idadedecachorro/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.elis.idadedecachorro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText caixaTexto;
+    private Button botaoIdade;
+    private TextView resultadoIdade;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        caixaTexto = findViewById(R.id.caixaTextoIdadeId);
+        botaoIdade = findViewById(R.id.botaoIdadeId);
+        resultadoIdade = findViewById(R.id.resultadoIdadeId);
+
+        botaoIdade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String numeroDigitado = caixaTexto.getText().toString();
+
+                if (numeroDigitado.isEmpty()) {
+
+                    resultadoIdade.setText("Nenhum número digitado !!");
+
+                } else {
+
+                    int valorDigitado = Integer.parseInt(numeroDigitado);
+                    int resultadoTotal = valorDigitado * 7;
+
+                    resultadoIdade.setText("A idade do cachorro em anos humanos é: " + resultadoTotal + " anos");
+                }
+            }
+        });
+    }
+}
